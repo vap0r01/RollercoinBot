d06ad3258fc2942ed620f069638fd6538ef4a684
SteidleR <robst01@web.de>
2020-12-30 15:28:50 +0100

    first implementation of coinflip not working test implementation

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 import time
 import datetime
@@ -8,9 +9,17 @@ import keyboard
 import mouse
 import shutil
 from PIL import ImageGrab
+from MTM import matchTemplates
 
 GAME_NUM = 0
 START_TIME = datetime.datetime.now()
+
+
+def mouse_click(x, y, wait=0.2):
+    m_pos = mouse.get_position()
+    mouse.drag(m_pos[0], m_pos[1], x, y, absolute=True, duration=0)
+    time.sleep(wait)
+    mouse.click("left")
 
 
 def screen_grab():
@@ -25,11 +34,11 @@ def find_image(image_path, root_image_path):
     root_image = cv2.imread(root_image_path)
     method = [cv2.TM_SQDIFF_NORMED, cv2.TM_CCOEFF][0]
     result = cv2.matchTemplate(image, root_image, method)
-    mn, _, mnLoc, _ = cv2.minMaxLoc(result)
+    mn, _, mn_loc, _ = cv2.minMaxLoc(result)
     if mn > 0.01:
         return None, None
-    MPx, MPy = mnLoc
-    return MPx, MPy
+    mp_x, mp_y = mn_loc
+    return mp_x, mp_y
 
 
 def check_image(img):
@@ -42,14 +51,11 @@ def click_image(img):
     x, y = find_image(img, screen_grab())
     if x is None or y is None:
         return
-    mpos = mouse.get_position()
 
     im = cv2.imread(img)
-    trows, tcols = im.shape[:2]
+    t_rows, t_cols = im.shape[:2]
 
-    mouse.drag(mpos[0], mpos[1], x + tcols * (3 / 5), y + trows * (2 / 3), absolute=True, duration=0)
-    time.sleep(0.2)
-    mouse.click("left")
+    mouse_click(x + t_rows * (3 / 5), y + t_cols * (2 / 3))
 
 
 def setup():
@@ -60,16 +66,15 @@ def setup():
 
 
 def start_game(start_img_path):
+    print("Starting")
     while not check_image(start_img_path):
         time.sleep(5)
     click_image(start_img_path)
-
+    print("Start game")
     while not check_image("rc_items/start_game.png"):
         time.sleep(1)
     sx, sy = find_image("rc_items/start_game.png", screen_grab())
-    mpos = mouse.get_position()
-    mouse.drag(mpos[0], mpos[1], sx + 2, sy + 2, absolute=True, duration=0)
-    mouse.click("left")
+    mouse_click(sx + 2, sy + 2, wait=0.1)
     time.sleep(3)
 
 
@@ -114,17 +119,112 @@ class Bot2048:
                 time.sleep(0.25)
 
 
+class BotCoinFlip:
+    def __init__(self):
+        self.start_img_path = "rc_items/coinflip_gameimg.png"
+        self.game = "CoinFlip"
+        self.coin_pos = []
+        self.coin_item = {
+            "binance": [],
+            "btc": [],
+            "eth": [],
+            "litecoin": [],
+            "monero": [],
+            "eos": [],
+        }
+
+    def play(self):
+        start_game(self.start_img_path)
+        start_game_msg(self.game)
+        self.get_coin_fields()
+        self.check_coins()
+        self.match_coins()
+        end_game()
+
+    def get_coin_fields(self):
+        matches = matchTemplates(
+            [("card", cv2.imread("rc_items/coinflip_back.png"))],
+            cv2.imread(screen_grab()),
+            N_object=float("inf"),
+            score_threshold=0.5,
+            maxOverlap=0.25,
+            searchBox=None)['BBox']
+        for i in range(len(matches)):
+            self.coin_pos.append(matches[i])
+
+    def check_coins(self):
+        print(self.coin_pos, len(self.coin_pos))
+        pos = 0
+        max_index = len(self.coin_pos)-1
+        while pos < max_index:
+            c1 = self.coin_pos[pos]
+            c1_item = None
+            mouse_click(c1[0] + c1[2]/2, c1[1] + c1[3]/2, wait=0.05)
+            for coin in self.coin_item.keys():
+                match = matchTemplates(
+                    [("card", cv2.imread(f"rc_items/coinflip_item_{coin}.png"))],
+                    cv2.imread(screen_grab()),
+                    N_object=float("inf"),
+                    score_threshold=0.5,
+                    maxOverlap=0.25,
+                    searchBox=c1)['BBox']
+                if len(match) == 1:
+                    c1_item = coin
+                    self.coin_item[c1_item].append(c1)
+                    break
+            pos += 1
+            time.sleep(0.3)
+
+            c2 = self.coin_pos[pos]
+            c2_item = None
+            mouse_click(c2[0] + c2[2] / 2, c2[1] + c2[3] / 2, wait=0.05)
+            for coin in self.coin_item.keys():
+                match = matchTemplates(
+                    [("card", cv2.imread(f"rc_items/coinflip_item_{coin}.png"))],
+                    cv2.imread(screen_grab()),
+                    N_object=float("inf"),
+                    score_threshold=0.5,
+                    maxOverlap=0.25,
+                    searchBox=c2)['BBox']
+                if len(match) == 1:
+                    c2_item = coin
+                    self.coin_item[c1_item].append(c1)
+                    break
+            pos += 1
+            time.sleep(0.3)
+
+            if not c1_item:
+                print("An error occurred: No matching coin! C1 missing")
+                sys.exit()
+
+            if not c2_item:
+                print("An error occurred: No matching coin! C2 missing")
+                sys.exit()
+
+            if c1_item == c2_item:
+                self.coin_item.pop(c1_item)
+
+            time.sleep(0.7)
+
+    def match_coins(self):
+        for coin in self.coin_item.values():
+            c1 = coin[0]
+            mouse_click(c1[0] + c1[2] / 2, c1[1] + c1[3] / 2, wait=0.05)
+            c2 = coin[1]
+            mouse_click(c2[0] + c2[2] / 2, c2[1] + c2[3] / 2, wait=0.05)
+            time.sleep(2)
+
+
 def main():
     global GAME_NUM
     while True:
         GAME_NUM += 1
-        Bot2048().play()
+        BotCoinFlip().play()
         time.sleep(20)
 
 
 if __name__ == "__main__":
     setup()
-
     try:
         main()
 
@@ -136,4 +236,4 @@ if __name__ == "__main__":
               "Time running: {!s}\n".format(datetime.datetime.now()-START_TIME),
               "Played Games:  {!s}\n".format(GAME_NUM)
               )
-        shutil.rmtree('imgs')
+        # shutil.rmtree('imgs')
